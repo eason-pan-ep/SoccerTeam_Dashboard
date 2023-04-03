52ebfa77ed452cfea9c513c6e4a2fdea9b0e45ef
Eason Pan <eason.pan@proton.me>
2023-04-03 13:39:32 -0700

    updated the design diagram of the Model.Completed U10TeamModel Interface.

--- Final_Project/src/U10Team/U10TeamModel.java
+++ Final_Project/src/U10Team/U10TeamModel.java
@@ -1,5 +1,41 @@
 package U10Team;
 
 public interface U10TeamModel {
-  void addPlayer(String firstName, String lastName, )
+  /**
+   * Add a new Player to the team, assign an unused jersey number to this Player when successfully added this Player.
+   * @param firstName first name of the added player.
+   * @param lastName last name of the added player.
+   * @param year the year of birth of the added player.
+   * @param month the month of birth of the added player.
+   * @param day the day of birth of the added player.
+   * @param preferredPosition the preferred position of the added player.
+   * @param skillLevel the skill level a coach provides for the added player.
+   * @throws IllegalArgumentException when the year is more than 10 years before the current year or later than current year,
+   *              when the month is less than 1 or greater than 12,
+   *              when the day is less than 1 or greater than 31,
+   *              when skillLevel is less than 0 or greater than 10.
+   * @throws IllegalStateException when the adding player is order than 10 years old,
+   *              when the team is at full capacity (20 Players).
+   */
+  void addPlayer(String firstName, String lastName, int year, int month, int day, Position preferredPosition, int skillLevel) throws IllegalArgumentException, IllegalStateException;
+
+  /**
+   * Create a team and auto generate the staring lineup.
+   * @throws IllegalStateException when there are less than 10 Players in the team.
+   */
+  void createTeam()throws IllegalStateException;
+
+  /**
+   * Get a list-like string with every Player in the team with the information of the Player's first name, last name, jersey number.
+   * This List is sorted in alphabetical order (last name).
+   * @return a list-like string contains the information described above.
+   */
+  String getTeam();
+
+  /**
+   * Get a list-like string with every Player in the starting lineup with the information of: first name, last name, jersey number, and position.
+   * This list is sorted by Position(goalie, defender, midfielder, forward).
+   * @return a list-like string contains the information described above.
+   */
+  String getStartingLineup();
 }
